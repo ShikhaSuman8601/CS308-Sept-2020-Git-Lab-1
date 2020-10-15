8dd6a44eae01fa484134e7ab431b88ee60937126
ShikhaSuman8601 <shikhadarshi@gmail.com>
2020-10-15 22:25:31 +0530

    Added NEGSUPPORT prefix in sqrt.c

--- sqrt.c
+++ sqrt.c
@@ -9,19 +9,19 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: ./sqrt 'input'\n\n");
+		printf("NEGSUPPORT: Usage: ./sqrt 'input'\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if (input<0) {
 		int input2=input * (-1);
-		printf("Square Root of %d is %fi\n", input, sqrt(input2));
-		printf("Exiting\n");
+		printf("NEGSUPPORT: Square Root of %d is %fi\n", input, sqrt(input2));
+		printf("NEGSUPPORT: Exiting\n");
 		return 0;
 	}
-	printf("Square Root of %d is %f\n",input,sqrt(input));
-	printf("End of program. Exiting\n");
+	printf("NEGSUPPORT: Square Root of %d is %f\n",input,sqrt(input));
+	printf("NEGSUPPORT: End of program. Exiting\n");
 	return(0);
 
 } // end main
