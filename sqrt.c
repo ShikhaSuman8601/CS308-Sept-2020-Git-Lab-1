d6c33aa7adc0c012b56294c2153482a0587213a4
ShikhaSuman8601 <shikhadarshi@gmail.com>
2020-10-15 21:54:18 +0530

    Added Negative Support

--- sqrt.c
+++ sqrt.c
@@ -14,7 +14,12 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
-	if (input<0) {printf("Invalid Number\n"); exit(-1);}
+	if (input<0) {
+		int input2=input * (-1);
+		printf("Square Root of %d is %fi\n", input, sqrt(input2));
+		printf("End of program. Exiting\n");
+		return 0;
+	}
 	printf("Square Root of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting\n");
 	return(0);
